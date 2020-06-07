c3da74294b22dd59f3e4e6b1d1c6e3a99ea1bfd7
oleksadnr kostyuk <morethanoleksandr@gmail.com>
2020-06-07 23:22:16 +0200

    aded adaptiv part 2

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-    
+$(function () {
+
 
 
     $('.rate__star').rateYo({
@@ -13,6 +13,29 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
@@ -23,19 +46,19 @@ $(function(){
         to: 600,
         prefix: "$"
     });
-    
-    $('.icon-th-list').on('click',function () {
+
+    $('.icon-th-list').on('click', function () {
         $('.product__item').addClass('list');
         $('.icon-th-list').addClass('active')
         $('.icon-th-large').removeClass('active');
     });
-    $('.icon-th-large').on('click',function () {
+    $('.icon-th-large').on('click', function () {
         $('.product__item').removeClass('list')
         $('.icon-th-large').addClass('active')
         $('.icon-th-list').removeClass('active');
     });
-    $('.menu__btn').on('click',function () {
-       $('.menu__list').slideToggle(); 
+    $('.menu__btn').on('click', function () {
+        $('.menu__list').slideToggle();
     });
     $('.header__btn-menu').on('click', function () {
         $('.header__box').toggleClass('active');
